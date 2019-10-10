878142c8c06f76e5410197e8a02ff5f91b8c1ac7
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-10-10 17:52:16 -0400

    Completed MVP

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Characters from "./components/Characters";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="ui centered">
+        <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
       </header>
+      <Characters />
     </div>
   );
 }
